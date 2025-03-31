ceb76ccbd23ad5aaa46139399f5140807576d8e2
88yhtserof <88yhtserofg@gmail.com>
2025-03-31 21:21:27 +0900

    [FEAT] TitleSupplementaryView 구현 #3

---
+++ TomeLink/TomeLink/Presentation/DesignSystem/TitleSupplementaryView.swift
@@ -0,0 +1,74 @@
+//
+//  TitleSupplementaryView.swift
+//  TomeLink
+//
+//  Created by 임윤휘 on 3/31/25.
+//
+
+import UIKit
+
+import SnapKit
+
+final class TitleSupplementaryView: UICollectionReusableView, BaseSupplementaryView {
+    
+    static var elementKind = String(describing: TitleSupplementaryView.self)
+    
+    private let titleLabel = UILabel()
+    let accessoryButton = UIButton()
+    
+    var accessoryButtonConfiguration: UIButton.Configuration? {
+        get { accessoryButton.configuration }
+        set {
+            accessoryButton.isHidden = false
+            accessoryButton.configuration = newValue
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureViews()
+        configureHierarchy()
+        configureConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        accessoryButton.isHidden = true
+    }
+    
+    func configure(with string: String) {
+        titleLabel.text = string
+    }
+}
+
+//MARK: - Configuration
+private extension TitleSupplementaryView {
+    private func configureViews() {
+        
+        titleLabel.font = .systemFont(ofSize: 16, weight: .bold)
+        titleLabel.textColor = TomeLinkColor.title
+        
+        accessoryButton.isHidden = true
+    }
+    
+    private func configureHierarchy() {
+        addSubviews(titleLabel, accessoryButton)
+    }
+    
+    private func configureConstraints() {
+        titleLabel.snp.makeConstraints { make in
+            make.verticalEdges.leading.equalToSuperview().inset(10)
+        }
+        
+        accessoryButton.snp.makeConstraints { make in
+            make.centerY.equalTo(titleLabel)
+            make.trailing.equalToSuperview().inset(10)
+        }
+    }
+}
+
