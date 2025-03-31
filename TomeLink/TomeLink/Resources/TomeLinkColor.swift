4c4557b73b4c98a9f1fd08f639d4c757599dbb40
88yhtserof <88yhtserofg@gmail.com>
2025-03-31 16:30:13 +0900

    [ADD/FEAT] ColorAsset 추가 및 Color 시스템 구성 #3

---
+++ TomeLink/TomeLink/Resources/TomeLinkColor.swift
@@ -0,0 +1,16 @@
+//
+//  TomeLinkColor.swift
+//  TomeLink
+//
+//  Created by 임윤휘 on 3/31/25.
+//
+
+import UIKit
+
+enum TomeLinkColor {
+    
+    static let title: UIColor = .tomelinkBlack
+    static let subtitle: UIColor = .tomelinkGrayDark
+    static let point: UIColor = .tomelinkPoint
+    static let shadow: UIColor = .tomelinkGray
+}
