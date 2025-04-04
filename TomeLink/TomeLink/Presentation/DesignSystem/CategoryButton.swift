c0c71c525307669ebb35b769e1fe909b6bd8d45d
88yhtserof <88yhtserofg@gmail.com>
2025-04-04 17:09:59 +0900

    [FEAT] 카테고리 목록 구현 #6

---
+++ TomeLink/TomeLink/Presentation/DesignSystem/CategoryButton.swift
@@ -0,0 +1,70 @@
+//
+//  CategoryButton.swift
+//  TomeLink
+//
+//  Created by 임윤휘 on 4/4/25.
+//
+
+import UIKit
+
+final class CategoryButton: UIButton {
+    
+    private var normalConfiguration = UIButton.Configuration.plain()
+    private var selectedConfiguration = UIButton.Configuration.filled()
+    
+    var title: String? {
+        get { self.currentTitle }
+        set {
+            var attributedString = AttributedString(newValue ?? "")
+            attributedString.font = .systemFont(ofSize: 14, weight: .bold)
+            normalConfiguration.attributedTitle = attributedString
+            selectedConfiguration.attributedTitle = attributedString
+            configuration?.attributedTitle = attributedString
+        }
+    }
+    
+    init() {
+        super.init(frame: .zero)
+        
+        configureView()
+        
+        configurationUpdateHandler = { [weak self] button in
+            
+            switch button.state {
+            case .normal:
+                self?.configuration = self?.normalConfiguration
+            case .selected:
+                self?.configuration = self?.selectedConfiguration
+            default:
+                break
+            }
+        }
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        isSelected.toggle()
+    }
+}
+
+//MARK: - Configuration
+private extension CategoryButton {
+    
+    func configureView() {
+        
+        cornerRadius(4)
+        border()
+        
+        normalConfiguration.baseForegroundColor = TomeLinkColor.title
+        normalConfiguration.baseBackgroundColor = TomeLinkColor.point
+        
+        selectedConfiguration.baseForegroundColor = TomeLinkColor.imagePlaceholder
+        selectedConfiguration.baseBackgroundColor = TomeLinkColor.title
+    }
+}
---
+++ TomeLink/TomeLink/Presentation/DesignSystem/CategoryCollectionViewCell.swift
@@ -0,0 +1,62 @@
+//
+//  CategoryCollectionViewCell.swift
+//  TomeLink
+//
+//  Created by 임윤휘 on 4/4/25.
+//
+
+import UIKit
+import SnapKit
+
+class CategoryCollectionViewCell: UICollectionViewCell, BaseCollectionViewCell {
+    
+    static let identifier = String(describing: CategoryCollectionViewCell.self)
+    
+    // Views
+    private let categoryButton = CategoryButton()
+    
+    // LifeCycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureHierarchy()
+        configureConstraints()
+        configureView()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // Feature
+    func configure(with value: (title: String, isSelected: Bool)) {
+        
+        categoryButton.title = value.title
+        categoryButton.isSelected = value.isSelected
+    }
+}
+
+//MARK: - Configuration
+private extension CategoryCollectionViewCell {
+    
+    func configureView() {
+        
+        contentView.cornerRadius()
+    }
+    
+    func configureHierarchy() {
+        contentView.addSubviews(categoryButton)
+    }
+    
+    func configureConstraints() {
+        
+        let verticalInset: CGFloat = 4
+        let horizontalInset: CGFloat = 2
+        
+        categoryButton.snp.makeConstraints { make in
+            make.verticalEdges.equalToSuperview().inset(verticalInset)
+            make.horizontalEdges.equalToSuperview().inset(horizontalInset)
+        }
+    }
+}
