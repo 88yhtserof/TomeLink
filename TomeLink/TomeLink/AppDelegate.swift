bc5753dfb990fed0c9d1c51c9c6f8a36951562c1
88yhtserof <88yhtserofg@gmail.com>
2025-04-08 09:05:33 +0900

    [FEAT] navigation bar backbutton title 숨김 #10

--- TomeLink/TomeLink/AppDelegate.swift
+++ TomeLink/TomeLink/AppDelegate.swift
@@ -76,8 +76,17 @@ extension  AppDelegate {
     
     func configureNavigationBarAppearance() {
         
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backButtonAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.clear]
+        appearance.backButtonAppearance.highlighted.titleTextAttributes = [.foregroundColor: UIColor.clear]
+        
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        
         UINavigationBar.appearance().tintColor = TomeLinkColor.title
-        UINavigationBar.appearance().backIndicatorImage = UIImage(systemName: "arrow.left")
-        UINavigationBar.appearance().backIndicatorTransitionMaskImage = UIImage(systemName: "arrow.left")
+        UINavigationBar.appearance().backIndicatorImage = UIImage(systemName: "chevron.left")
+        UINavigationBar.appearance().backIndicatorTransitionMaskImage = UIImage(systemName: "chevron.left")
+
     }
 }
